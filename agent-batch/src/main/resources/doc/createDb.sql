8c95c82fa58b33a56a96755c59a66e0ededb3ce2
eden <sjtuzhaoxh@163.com>
2017-10-30 21:07:52 +0800

    update sql

--- agent-batch/src/main/resources/doc/createDb.sql
+++ agent-batch/src/main/resources/doc/createDb.sql
@@ -1,28 +1,38 @@
-CREATE DATABASE trans_video;
-
-USE trans_video;
-
-CREATE TABLE BASE_INFO
+create table trans_video.base_info
 (
-  ID BIGINT NOT NULL AUTO_INCREMENT COMMENT '自增ID' PRIMARY KEY ,
-  KEY_WORD VARCHAR(64) NOT NULL DEFAULT '0' COMMENT '关键字',
-  PAGE_NUM INT NOT NULL DEFAULT 0 COMMENT '页数',
-  URL VARCHAR(100) NOT NULL DEFAULT '0' COMMENT 'YouTube地址',
-  VIDEO_LINKS VARCHAR(4096) NOT NULL DEFAULT 0 COMMENT '视频链接',
-  VALID INT NOT NULL DEFAULT 0 COMMENT '是否有效，0无效，1有效',
-  PUBLISH_STATUS INT NOT NULL DEFAULT 0 COMMENT '发布状态，0未发布，1已发布',
-  COLLECT_TIME TIMESTAMP  NOT NULL DEFAULT '1990-01-01 00:000:00' COMMENT '采集时间',
-  PUBLISH_TIME TIMESTAMP  NOT NULL DEFAULT '1990-01-01 00:000:00' COMMENT '发布时间',
-  CREATE_TIME TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '创建时间',
-  UPDATE_TIME TIMESTAMP  NOT NULL DEFAULT '1990-01-01 00:000:00' COMMENT '更新时间',
-  INFO_HASH VARCHAR(64) NOT NULL DEFAULT '0' COMMENT 'hash',
-  FETCH_STATUS INT NOT NULL DEFAULT 0 COMMENT '抓取状态：0初始状态，1抓取成功，2抓取失败',
-  CRAW_STATUS INT NOT NULL DEFAULT 0 COMMENT '抓取状态',
-  VIDEO_TITLE VARCHAR(1024) NOT NULL DEFAULT '0' COMMENT '视频名称',
-  VIDEO_INFO VARCHAR(2048) NOT NULL DEFAULT '0' COMMENT '获取视频信息',
-  VIDEO_ITEM_ID VARCHAR(64) NOT NULL DEFAULT '0' COMMENT 'topbuzz网视频ID',
-  POST_STATUS INT NOT NULL DEFAULT 0 COMMENT 'post状态：0初始状态，1抓取成功，2抓取失败'
+	ID bigint auto_increment comment '自增ID'
+		primary key,
+	KEY_WORD varchar(64) default '0' not null comment '关键字',
+	PAGE_NUM int default '0' not null comment '页数',
+	URL varchar(100) default '0' not null comment 'YouTube地址',
+	VIDEO_LINKS varchar(4096) default '0' not null comment '视频链接',
+	VALID int default '0' not null comment '是否有效，0无效，1有效',
+	PUBLISH_STATUS int default '0' not null comment '发布状态，0未发布，1已发布',
+	COLLECT_TIME timestamp default '1990-01-01 00:00:00' not null comment '采集时间',
+	PUBLISH_TIME timestamp default '1990-01-01 00:00:00' not null comment '发布时间',
+	CREATE_TIME timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
+	UPDATE_TIME timestamp default '1990-01-01 00:00:00' not null comment '更新时间',
+	INFO_HASH varchar(64) default '0' not null comment 'hash',
+	FETCH_STATUS int default '0' not null comment '抓取状态：0初始状态，1抓取成功，2抓取失败',
+	CRAW_STATUS int default '0' not null comment '抓取状态',
+	VIDEO_TITLE varchar(1024) default '0' not null comment '视频名称',
+	VIDEO_INFO varchar(2048) default '0' not null comment '获取视频信息',
+	VIDEO_ITEM_ID varchar(64) default '0' not null comment 'topbuzz网视频ID',
+	POST_STATUS int default '0' not null comment 'post状态：0初始状态，1抓取成功，2抓取失败'
+)
+;
 
-) ;
+create index info_hash
+	on base_info (INFO_HASH)
+;
 
-CREATE INDEX info_hash ON BASE_INFO(INFO_HASH);
+create table trans_video.task_info
+(
+	ID bigint auto_increment comment '自增ID'
+		primary key,
+	TASK_NAME varchar(64) default '0' not null comment '任务名称',
+	TASK_STATUS int default '0' not null comment '任务状态',
+	TASK_DISP varchar(64) default '初始化' not null comment '任务状态显示'
+)
+;
+
